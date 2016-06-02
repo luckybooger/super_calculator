54ed84923fd1d41ad702017ce1ffc9579e233a08
Lucy Berger and Valerie Conarroe <vconarro33@gmail.com>
2016-06-01 20:57:32 -0700

    include subtraction, multiplication and division

--- js/scripts.js
+++ js/scripts.js
@@ -22,5 +22,15 @@ $(function(){
   var number2 = parseInt($("#add2").val());
   var result = add(number1, number2);
   $("#output").text(result);
+
+  $("form#subtract").submit(function(event) {
+  event.preventDefault();
+
+  var number1 = parseInt($("#subtract1").val());
+  var number2 = parseInt($("#subtract2").val());
+  var result = subtract(number1, number2);
+  $("#output").text(result);
+
+  });
   });
 });
